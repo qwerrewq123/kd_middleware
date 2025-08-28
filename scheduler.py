483265b42ba7b7ce5d94109fabc44fd53f50e2d2
qwerrewq123 <preejer8013@gmail.com>
2025-08-28 15:15:12 +0900

    synchronization logic

--- scheduler.py
+++ scheduler.py
@@ -8,10 +8,10 @@ from fcm_dto import FcmDto
 from push_fcm import PushFcm
 from push_sql import PushSql
 import toml
-from utils import get_resource_path
+from utils import get_resource_path,get_sync_rows,process_to_tuple
 
 from mysql_connector import MySQLConnector
-
+from mssql_connector import MSSQLConnector
 logger = logging.getLogger(__name__)
 
 
@@ -25,6 +25,7 @@ class Scheduler:
             config = toml.load(f)
             logger.info(f"Load config.toml file from {config_path}")
         mysql_config = config['database']['mariadb']
+        mssql_config = config['database']['mssql']
         self.interval = interval
         self.is_running = False
         self.thread: Optional[threading.Thread] = None
@@ -40,6 +41,17 @@ class Scheduler:
         else:
             logger.info('MYSQL Connection Test Fail')
         logger.info("MYSQL Connector Successful")
+        self.mssql_connector: MSSQLConnector = MSSQLConnector(
+            host=mssql_config['host'],
+            port=mssql_config['port'],
+            database=mssql_config['database'],
+            username=mssql_config['username'],
+            password=mssql_config['password']
+        )
+        if self.mssql_connector.test_connection():
+            logger.info('MSSQL Connection Test Success')
+        else:
+            logger.info('MSSQL Connection Test Fail')
         self.push_sql: PushSql = PushSql()
         self.push_fcm: PushFcm = PushFcm()
 
@@ -48,30 +60,53 @@ class Scheduler:
         
     def job(self):
         """Job to execute"""
-        try:
-            self.mysql_connector.start_transaction()
-            result = self.mysql_connector.execute_query(query=self.push_sql.select_query)
-            idx_list = [res['idx'] for res in result]
-            count = len(idx_list)
-            logger.info(f'count is {count}')
-            logger.info(f'interval is {self.interval}')
-            if count > 0:
-                try:
-                    self.mysql_connector.execute_update(query=self.push_sql.fcm_query)
-                    rows = self.mysql_connector.execute_query(query=self.push_sql.fcm_select_query)
-                    fcm_list = [FcmDto(row['TOKEN'], row['TITLE'], row['CONTENT']) for row in rows]
-                    docno_list = [row['DOCNO'] for row in rows]
-                    self.push_fcm.push(fcm_list)
-                    for docno, idx in zip(docno_list, idx_list):
-                        self.mysql_connector.execute_update(self.push_sql.alarm_fcm_update_query, params=(docno,))
-                        self.mysql_connector.execute_update(self.push_sql.alarm_event_update_query, params=(idx,))
-                except Exception as e:
-                    logger.error(f"Fcm Query Execution Fail: {e}")
-                    if self.mysql_connector.connection:
-                        self.mysql_connector.connection.rollback()
-            self.mysql_connector.commit()
-        except Exception as e:
-            self.mysql_connector.rollback()
+        logger.info('check synchronization')
+        sync_source_result = self.mssql_connector.execute_query(self.push_sql.alarm_event_synchronize_query_source)
+        sync_target_result = self.mysql_connector.execute_query(self.push_sql.alarm_event_synchronize_query_target)
+        source_cnt = len(sync_source_result)
+        target_cnt = len(sync_target_result)
+        logger.info(f'source cnt is {source_cnt}')
+        logger.info(f'target cnt is {target_cnt}')
+
+        if source_cnt > target_cnt:
+            try:
+                logger.info(f'source cnt is more than target cnt(source_cnt = {source_cnt}, target_cnt = {target_cnt})')
+                logger.info('synchronization....')
+                result = get_sync_rows(sync_source_result, target_cnt)
+                result_tup = process_to_tuple(result)
+                self.mysql_connector.start_transaction()
+                for tup in result_tup:
+                    self.mysql_connector.execute_update(self.push_sql.alarm_event_synchronize_insert_query, tup)
+                self.mysql_connector.commit()
+            except Exception as e:
+                logger.error(f"Synchronization Query Execution Fail: {e}")
+                if self.mysql_connector.connection:
+                    self.mysql_connector.connection.rollback()
+
+        time.sleep(0.5)
+        logger.info('check fcm')
+        result = self.mysql_connector.execute_query(query=self.push_sql.select_query)
+        idx_list = [res['idx'] for res in result]
+        count = len(idx_list)
+        logger.info(f'count is {count}')
+        logger.info(f'interval is {self.interval}')
+        if count > 0:
+            try:
+                self.mysql_connector.start_transaction()
+                self.mysql_connector.execute_update(query=self.push_sql.fcm_query)
+                rows = self.mysql_connector.execute_query(query=self.push_sql.fcm_select_query)
+                fcm_list = [FcmDto(row['TOKEN'], row['TITLE'], row['CONTENT']) for row in rows]
+                docno_list = [row['DOCNO'] for row in rows]
+                self.push_fcm.push(fcm_list)
+                for docno, idx in zip(docno_list, idx_list):
+                    self.mysql_connector.execute_update(self.push_sql.alarm_fcm_update_query, params=(docno,))
+                    self.mysql_connector.execute_update(self.push_sql.alarm_event_update_query, params=(idx,))
+                self.mysql_connector.commit()
+            except Exception as e:
+                logger.error(f"Fcm Query Execution Fail: {e}")
+                if self.mysql_connector.connection:
+                    self.mysql_connector.connection.rollback()
+
 
     def run_continuously(self):
         """Schedule in Background"""
--- utils.py
+++ utils.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 def get_resource_path(relative_path):
     """
     PyInstaller로 빌드된 실행 파일과 개발 환경 모두에서 
@@ -14,3 +13,37 @@ def get_resource_path(relative_path):
         base_path = os.path.abspath(".")
     
     return os.path.join(base_path, relative_path)
+
+def get_sync_rows(result,count):
+    total_cnt = len(result)
+    fetch_cnt = total_cnt - count
+    total_list = []
+    for i in range(fetch_cnt):
+        total_list.append(result[-1-i])
+    total_list.reverse()
+    return total_list
+
+def fix_kr(s):
+    return s.encode('cp1252', errors='replace').decode('cp949', errors='replace')
+
+def contains_leak(s):
+    return '누수' in s
+
+def process_to_tuple(results):
+    for result in results:
+        del result['idx']
+        result['END_TIME'] = None
+        result['TAG_DESC'] = fix_kr(result['TAG_DESC'])
+        if contains_leak(result['TAG_DESC']):
+            result['ALRAM_TP1_CD'] = 'TP002'
+            result['ALRAM_TP1_NM'] = '누수'
+        else:
+            result['ALRAM_TP1_CD'] = 'TP001'
+            result['ALRAM_TP1_NM'] = '가스'
+        result['ALRAM_MEMO'] = None
+        result['UPDATE_USER'] = None
+        result['CHECK_YN'] = 'N'
+        result['PUSH_YN'] = 'N'
+        result['UPDATE_TIME'] = None
+
+    return tuple(results)
